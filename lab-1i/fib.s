70c5162565b8f1b67aa497fed8eeaa6b7d42796f
xero7689 <volleyp7689@gmail.com>
2015-09-28 21:48:36 +0800

    I didn't finish this homework at the summer course, so I try to make it complete this time.
    1. Added ARM Machine Directives.
    2. Completed the calling convention.
    3. Removed redudant instruction.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -1,45 +1,59 @@
-/*
-Fibonacci Sequence Arm Assembly Implementation
-(Non-Recursive version)
-Unfinish yet.
-*/
+@Fibonacci Sequence Arm Assembly Implementation
+@(Non-Recursive version)
 
-.global _start
-_start:
-    mov r1, #0  /* f(0) */
-    mov r2, #1  /* f(1) */
-    mov r3, #2  /* index of i */
-    mov r4, #12 /* index of fibo */
+    .syntax unified
+    .arch armv7-a
+    .text
+    .align 2
+    .thumb
+    .thumb_func
+
+    .global fibonacci
+    .type fibonacci, function
+
+fibonacci:
+    push {r3, r4, r5, lr}    
+
+    mov r1, #0  @f(0)
+    mov r2, #1  @f(1)
+    mov r3, #2  @loop index
+    sub r4, r0, #0
     
-    /*
-    if (index == 0)
-        return 0;
-    if (index == 1)
-        return 1;
-    */
+    @if (index == 0)
+    @    return 0;
     cmp r1, r4
+    it eq
     moveq r0, r1
     beq exit
 
+    @if (index == 1)
+    @    return 1;
     cmp r2, r4
+    it eq
     moveq r0, r2
     beq exit
 
     add r4, r4, #1
 
-loop_cond:
+@ Loop condition testing
+.loop_cond:
+    @ if(index < parameter)
     cmp r3, r4
-    blt loop
-    mov r0, r5  @ Save result to r0
+    blt .loop
+
+    @ Save result to r0
+    mov r0, r5      
     b exit
 
-loop:
+.loop:
     add r5, r1, r2
     mov r1, r2
     mov r2, r5
     add r3, #1
-    b loop_cond
+    b .loop_cond
 
 exit:
-    mov r7, #1
-    swi 0
+    pop {r3, r4, r5, pc}
+    
+    .size fibonacci, .-fibonacci
+    .end
